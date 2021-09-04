47521aec806d99ad3f0b380a1656d732673d8cbf
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2021-09-04 16:16:36 +0200

    test(product): Adding integration test for product endpoints

--- src/test/kotlin/com/jojo5716/budapp/restapi/RestapiApplicationTests.kt
+++ src/test/kotlin/com/jojo5716/budapp/restapi/RestapiApplicationTests.kt
@@ -1,12 +1,14 @@
 package com.jojo5716.budapp.restapi
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.jojo5716.budapp.restapi.domain.Product
+import com.jojo5716.budapp.restapi.service.ProductService
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
+import java.util.regex.Matcher
 
 @SpringBootTest
 class RestapiApplicationTests {
@@ -21,32 +24,45 @@ class RestapiApplicationTests {
     private lateinit var webApplicationContext: WebApplicationContext
 
     @Autowired
-    private lateinit var mapper:ObjectMapper
+    private lateinit var productService: ProductService
+
+    @Autowired
+    private lateinit var mapper: ObjectMapper
 
     private val mockMvc: MockMvc by lazy {
         MockMvcBuilders.webAppContextSetup(webApplicationContext)
             .alwaysDo<DefaultMockMvcBuilder>(MockMvcResultHandlers.print())
             .build()
     }
-    
+
     private val endpointBase = "/api/v1/products"
-    
+
     @Test
     fun findAll() {
-        val expectedBody = "[{'name':'Product 1','price':22.2},{'name':'Product 2','price':33.1}]"
+        val productsFromService: List<Product> = productService.findAll()
 
-        mockMvc.perform(MockMvcRequestBuilders.get(endpointBase))
+        assert(!productsFromService.isEmpty()) { "Should not be empty" }
+
+        val json: String = mockMvc.perform(MockMvcRequestBuilders.get(endpointBase))
             .andExpect(status().is2xxSuccessful)
-            .andExpect(content().json(expectedBody))
+            .andReturn().response.contentAsString
+
+        val products: List<Product> = mapper.readValue(json)
+
+        assertThat(productsFromService, Matchers.`is`(Matchers.equalTo(products)))
     }
-    
+
     @Test
     fun findById() {
-        val expectedResult = "{'name':'Product 1','price':22.2}"
-        
-        mockMvc.perform(MockMvcRequestBuilders.get("${endpointBase}/Product 1"))
+        val productsFromService: List<Product> = productService.findAll()
+
+        assert(!productsFromService.isEmpty()) { "Should not be empty" }
+
+        val product = productsFromService.first()
+
+        mockMvc.perform(MockMvcRequestBuilders.get("${endpointBase}/${product.name}"))
             .andExpect(status().is2xxSuccessful)
-            .andExpect(content().json(expectedResult))
+            .andExpect(jsonPath("$.name", Matchers.`is`(product.name)))
     }
 
     @Test
@@ -60,12 +76,93 @@ class RestapiApplicationTests {
     fun saveSuccessfully() {
         val product = Product(name = "Apple Iphone X", price = 1000.0)
 
-        val result:Boolean = mockMvc.perform(MockMvcRequestBuilders.post(endpointBase)
-            .content(mapper.writeValueAsBytes(product))
-            .contentType(MediaType.APPLICATION_JSON))
+        val result: Boolean = mockMvc.perform(
+            MockMvcRequestBuilders.post(endpointBase)
+                .body(product, mapper)
+        )
             .andExpect(status().is2xxSuccessful)
             .bodyTo(mapper)
 
-            assert(result)
+        assert(result)
+    }
+
+    @Test
+    fun saveFail() {
+        val productsFromService: List<Product> = productService.findAll()
+
+        assert(!productsFromService.isEmpty()) { "Should not be empty" }
+
+        val firstProduct = productsFromService.first()
+
+        val result: Boolean = mockMvc.perform(
+            MockMvcRequestBuilders.post(endpointBase)
+                .body(firstProduct, mapper)
+        )
+            .andExpect(status().is2xxSuccessful)
+            .bodyTo(mapper)
+
+        assert(!result) { "Should be false" }
+    }
+
+    @Test
+    fun updateSuccessfully() {
+        val productsFromService: List<Product> = productService.findAll()
+
+        assert(!productsFromService.isEmpty()) { "Should not be empty" }
+
+        val product = productsFromService.first().copy(price = 1000.0)
+
+        val result: Boolean = mockMvc.perform(
+            MockMvcRequestBuilders.put(endpointBase)
+                .body(product, mapper)
+        )
+            .andExpect(status().is2xxSuccessful)
+            .bodyTo(mapper)
+
+        assert(result)
+    }
+
+    @Test
+    fun updateFail() {
+        val product = Product(name = "Unavailable product", price = 123.45)
+
+        val result: Boolean = mockMvc.perform(
+            MockMvcRequestBuilders.put(endpointBase)
+                .body(product, mapper)
+        )
+            .andExpect(status().is2xxSuccessful)
+            .bodyTo(mapper)
+
+        assert(!result) { "Should be false" }
+    }
+
+    @Test
+    fun deleteByIdSuccessfully() {
+        val productsFromService: List<Product> = productService.findAll()
+
+        assert(!productsFromService.isEmpty()) { "Should not be empty" }
+
+        val product = productsFromService.last()
+
+        val result: Boolean = mockMvc.perform(
+            MockMvcRequestBuilders.delete("${endpointBase}/${product.name}")
+        )
+            .andExpect(status().is2xxSuccessful)
+            .bodyTo(mapper)
+
+        assert(result)
+    }
+
+    @Test
+    fun deleteByIdFail() {
+        val product = Product(name = "Unavailable product", price = 123.45)
+
+        val result: Boolean = mockMvc.perform(
+            MockMvcRequestBuilders.delete("${endpointBase}/${product.name}")
+        )
+            .andExpect(status().is2xxSuccessful)
+            .bodyTo(mapper)
+
+        assert(!result)
     }
 }
