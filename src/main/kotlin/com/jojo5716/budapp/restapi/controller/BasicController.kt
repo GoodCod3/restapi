d3fc131d255cee29d6689bbd6a2f1d16e75961fc
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2021-09-05 18:50:43 +0200

    chore(product): Adding swagger description for endpoints

--- src/main/kotlin/com/jojo5716/budapp/restapi/controller/BasicController.kt
+++ src/main/kotlin/com/jojo5716/budapp/restapi/controller/BasicController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 abstract class BasicController<T, ID>(private val basicCRUD: BasicCRUD<T, ID>) {
-    @ApiOperation("Get all entities")
+    @ApiOperation("Get all entities", notes = "Returns all active entities")
     @GetMapping
     fun findAll(): ResponseEntity<List<T>> {
         val entities = this.basicCRUD.findAll()
@@ -16,6 +16,7 @@ abstract class BasicController<T, ID>(private val basicCRUD: BasicCRUD<T, ID>) {
         return ResponseEntity.status(HttpStatus.OK).body(entities)
     }
 
+    @ApiOperation("Find a entity by id", notes = "Return an entity or empty response with a 204 http status")
     @GetMapping("/{id}")
     fun findById(@PathVariable id: ID): ResponseEntity<T> {
         val entity = this.basicCRUD.findById(id)
@@ -23,12 +24,15 @@ abstract class BasicController<T, ID>(private val basicCRUD: BasicCRUD<T, ID>) {
         return ResponseEntity.status(if (entity != null) HttpStatus.OK else HttpStatus.NO_CONTENT).body(entity)
     }
 
+    @ApiOperation("Create a entity", notes = "Return an entity")
     @PostMapping
     fun save(@Valid @RequestBody body: T) = ResponseEntity.status(HttpStatus.CREATED).body(this.basicCRUD.save(body))
 
+    @ApiOperation("Update an entity if it id is found", notes = "Return an entity")
     @PutMapping
     fun update(@RequestBody body: T) = this.basicCRUD.update(body)
 
+    @ApiOperation("Delete an entity if it id is found", notes = "Return an entity")
     @DeleteMapping("/{id}")
     fun deleteById(@PathVariable id: ID) = this.basicCRUD.deleteById(id)
 
